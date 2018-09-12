e0bd6d921576adb48165533173ffb4f2d39ef75b
TheFirstSaiyan <19hari98@gmail.com>
2018-09-12 22:44:25 +0530

    added some improvements to playing mechanic

--- public/sketch.js
+++ public/sketch.js
@@ -1,65 +1,107 @@
-let locations=[];
+let locations = [];
 let clientSocket;
+
 
 function setup() {
 	createCanvas(400, 400);
 	background(0);
-	clientSocket=io.connect('http://localhost:6677');
+	clientSocket = io.connect('http://localhost:6677');
 	//receive loaction data from any other client connected to the server
-	textRegion=createInput();
-	textRegion.position(125,425);
+	textRegion = createInput();
+	textRegion.position(125, 425);
+	//	clientSocket.on('guesser', guesserFunction);
+	alertBox = createInput();
+	//	clientSocket.on('role', roleFunction);
 
-	clientSocket.on('clearInstruction',screenClear);
-	clientSocket.on('positionData',replicate);
+	clientSocket.on('clearInstruction', screenClear);
+
+	clientSocket.on('positionData', replicate);
+	clientSocket.on('drawer', drawer);
+	clientSocket.on('guesser', guesser);
+	clientSocket.on('wordToDraw', showWhatToDraw)
+		//clientSocket.on('wordToDraw', drawWord);
+		//clientSocket.on('wordToDraw', well);
+		//clientSocket.on("showText", showText);
+
 }
 
-function replicate(data)
-{
-	strokeWeight(10);
-	stroke(50,0,100);
+function showText(data) {
+	strokeWeight(5);
+	stroke(255);
+	textSize(32);
+	text(data, 150, 50);
 
-	line(data.px,data.py,data.x,data.y);
 }
 
-function screenClear(data)
-{
+function showWhatToDraw(values) {
+	strokeWeight(5);
+	textAlign(CENTER);
+	stroke(255);
+	textSize(32);
+	text(values[1], 180, 50);
+
+
+}
+
+function drawer(data) {
+
+	alertBox.value(data);
+
+}
+
+function guesser(data) {
+	alertBox.value(data);
+}
+
+
+
+function drawWord(data) {
+	console.log("draw" + data);
 	background(0);
-	console.log("cleared screen");
 }
 
-function mouseReleased()
-{
-	locations=[];
+function replicate(data) {
+	strokeWeight(10);
+	stroke(50, 0, 100);
+	line(data.px, data.py, data.x, data.y);
+}
+
+function screenClear(data) {
+	background(0);
+	console.log("cleared screen" + data);
+	//clientSocket.emit('WhatIsMyRole', "not sure");
+
+}
+
+function mouseReleased() {
+	locations = [];
 }
 
 
-function mouseDragged()
-{
+function mouseDragged() {
 	strokeWeight(10);
 	stroke(255);
-	if(mouseIsPressed)
-	{
-		line(pmouseX,pmouseY,mouseX,mouseY);
+	if (mouseIsPressed) {
+		line(pmouseX, pmouseY, mouseX, mouseY);
 	}
-	let data=
-	{
-		px:pmouseX,
-		py:pmouseY,
-		x:mouseX,
-		y:mouseY
+	let data = {
+		px: pmouseX,
+		py: pmouseY,
+		x: mouseX,
+		y: mouseY
 	};
 	// send current values of location to the server
-	clientSocket.emit('positionData',data);
+	clientSocket.emit('positionData', data);
 }
-function keyPressed()
-{
-	if(keyCode==ENTER)
-	{
+
+function keyPressed() {
+	if (keyCode == ENTER) {
 		console.log(textRegion.value());
-		clientSocket.emit('guess',textRegion.value());
+		clientSocket.emit('guess', textRegion.value());
 
 	}
 }
+
 function draw() {
 
 }
--- server.js
+++ server.js
@@ -1,102 +1,162 @@
 console.log("server starting .....");
 
 
-const http=require('http');
-const fs=require('fs');
-const express=require('express');
-const socket =require('socket.io');
-const request=require('request');
+const fs = require('fs');
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+const request = require('request');
+
+
+let app = express();
+
+
 //one random word http://www.desiquintans.com/noungenerator?count=1
 //text file : http://www.desiquintans.com/downloads/nounlist/nounlist.txt
 // request("http://www.desiquintans.com/noungenerator?count=1",function(error,response,body){
 //
 //   console.log(body);
 // });
+let correct = "";
+let words = [];
+let clientList = [];
+let turns = {};
+let paired = {};
+let pairs = {};
+let server = app.listen(6677);
+let io = socket(server);
 
-let clientList=[];
-let turns={};
-let paired={};
-let pairs={};
-let app=express();
-let server=app.listen(6677);
-let io=socket(server);
+//read the file containing the words
+let content = fs.readFileSync("nouns.txt", 'utf8');
+
+
+words = content.toString().split('\n');
+//console.log(words[words.length - 2]);
+
 
 app.use(express.static('public'));
 
 //detect a new client
-io.sockets.on('connection',newConnection);
+io.sockets.on('connection', newConnection);
 
 
 
-function hookUp(socket,socketId)
-{
+function hookUp(socket, socketId) {
   let madeUp = false;
-  for(let id of Object.keys(paired))
-  {
-  if(id==socketId)
+  for (let id of Object.keys(paired)) {
+    if (id == socketId)
       continue;
-  else
-  {
-      if(paired[id]==false)
-      {
-        paired[id]=true;
-        paired[socketId]=true;
-        pairs[socketId]=id;
-        pairs[id]=socketId;
-        madeUp=true;
-        console.log( "[+] HOOK UP SUCCESSFUL : " +socketId +" hooked up with " + id);
+    else {
+      if (paired[id] == false) {
+        paired[id] = true;
+        paired[socketId] = true;
+        pairs[socketId] = id;
+        pairs[id] = socketId;
+        madeUp = true;
+        console.log("[+] HOOK UP SUCCESSFUL : " + socketId + " hooked up with " +
+          id);
 
         //at first set turns randomly
-        turns[socketId]="draw";
-        turns[pairs[socketId]]="guess";
+        turns[socketId] = "draw";
+        turns[pairs[socketId]] = "guess";
+        //choose a random word from list
+        correct = words[Math.ceil(Math.random() * (words.length - 2))].replace(
+          ' ', '');
+        console.log(socketId + "hello");
 
         //clear your screen when u have been connected to  a new player
-        socket.broadcast.to(pairs[socketId]).emit('clearInstruction','clear your screen dood');
-        socket.broadcast.to(socketId).emit('clearInstruction','clear your screen dood');
+
+        socket.broadcast.to(pairs[socketId]).emit('clearInstruction',
+          'clear your screen dood');
+
+
+        socket.emit('clearInstruction',
+          'clear your screen dood');
+
+        socket.broadcast.to(pairs[socketId]).emit('guesser', turns[pairs[
+          socketId]]);
+        socket.emit('drawer', "draw");
+        let flag = 0;
+        let ok = socket.emit("wordToDraw", [turns[socketId], correct], success);
+        //checking if its not the same client
+        function success(data) {
+          flag = 1;
+        }
+        if (flag === 0) {
+          socket.broadcast.to(socketId).emit('drawer',
+            "draw");
+          socket.broadcast.to(socketId).emit('clearInstruction',
+            'clear your screen dood');
+          socket.broadcast.to(socketId).emit("wordToDraw", [turns[
+            socketId], correct]);
+        }
+        socket.broadcast.to(pairs[socketId]).emit("wordToDraw", [turns[pairs[
+          socketId]], "your turn to guess"]);
+
 
       }
 
+    }
   }
-  }
-  if(madeUp==false)
-        console.log("[-] waiting for a hookup. No other players found online :(");
+  if (madeUp == false)
+    console.log("[-] waiting for a hookup. No other players found online :(");
 
 
 }
 
-function newConnection(socket)
-{
+
+
+function newConnection(socket) {
   let pair;
-  clientList.push(socket.id);         //keep  track of all the clients currently connected to the server
-  paired[socket.id]=false;
-  pairs[socket.id]=undefined;
+  clientList.push(socket.id); //keep  track of all the clients currently connected to the server
+  paired[socket.id] = false;
+  pairs[socket.id] = undefined;
   console.log("[+] new client connected with id :" + socket.id);
 
-  // this is where i 'hookup' different clients
+  // this is where I 'hookup' different clients
 
-    hookUp(socket,socket.id);
+  hookUp(socket, socket.id);
+  // for (let one of Object.keys(pairs)) {
+  //   if (turns[one] == "draw") {
+  //     socket.broadcast.to(one).emit("showText", correct);
+  //
+  //   } else if (turns[pairs[one]] == "guess") {
+  //
+  //     socket.broadcast.to(pairs[one]).emit("showText", correct);
+  //
+  //   }
+  //
+  // }
 
-console.log(socket.id + turns[socket.id]);
+
+
+  //  console.log(socket.id + turns[socket.id]);
 
   //receive location data from one client
-  socket.on('positionData',getData);
-  socket.on('guess',checkGuess);
+  socket.on('positionData', getData);
+  socket.on('guess', checkGuess);
+  //  socket.on('WhatIsMyRole', checkRole);
 
-  function getData(data)
-  {
+  function checkRole(data) {
+    socket.broadcast.to(socket.id).emit('role', turns[socket.id].toString());
+  }
+
+  function getData(data) {
     // send location data to a selected client only using my hookup mechanism
 
-    if(pairs[socket.id]!=undefined)
-    {
-      socket.broadcast.to(pairs[socket.id]).emit('positionData',data);
-    }
-    else
+    if (pairs[socket.id] != undefined) {
+      socket.broadcast.to(pairs[socket.id]).emit('positionData', data);
+    } else
       console.log("[-] waiting for a hookup. No other players found online :(");
   }
 
-  function checkGuess(guess)
-  {
+  function checkGuess(guess) {
     console.log(guess);
+    let chosenWord = "hari";
+    //check if the guess is correct !
+    if (guess == correct && turns[socket.id] == "guess") {
+      console.log("correct");
+    }
     //check if the guess is the right word or is a related word
     //and keep changing turns :)
     console.log(socket.id + turns[socket.id]);
@@ -105,17 +165,17 @@ console.log(socket.id + turns[socket.id]);
 
 
   // detect a leaving client
-    socket.on('disconnect',function (){
+  socket.on('disconnect', function() {
     console.log("[-] client disconnected with id :" + socket.id);
-    let indexToRemove=clientList.indexOf(socket.id);
-    paired[pairs[socket.id]]=false;
-    pairs[pairs[socket.id]]=undefined;
-    pair=pairs[socket.id];
-    pairs[socket.id]=undefined;
-    turns[socket.id]=undefined;
-    turns[pair]=undefined;
-    hookUp(socket,pair)
-    clientList.splice(indexToRemove,1);
+    let indexToRemove = clientList.indexOf(socket.id);
+    paired[pairs[socket.id]] = false;
+    pairs[pairs[socket.id]] = undefined;
+    pair = pairs[socket.id];
+    pairs[socket.id] = undefined;
+    turns[socket.id] = undefined;
+    turns[pair] = undefined;
+    hookUp(socket, pair)
+    clientList.splice(indexToRemove, 1);
     remainingClients();
   });
 
@@ -123,13 +183,11 @@ console.log(socket.id + turns[socket.id]);
 
 
 
-function remainingClients()
-{
+function remainingClients() {
 
   console.log("\n**********REMAINING CLIENTS***********\n");
-  for(let sid of clientList)
-  {
-    console.log(sid+'\n');
+  for (let sid of clientList) {
+    console.log(sid + '\n');
   }
 
 }
